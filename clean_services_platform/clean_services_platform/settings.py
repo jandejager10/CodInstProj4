e8cef01491123e1733f185ef7295712df186459d
jandejager10 <40761225+jandejager10@users.noreply.github.com>
2024-10-07 11:12:04 +0100

    add models except users

--- clean_services_platform/clean_services_platform/settings.py
+++ clean_services_platform/clean_services_platform/settings.py
@@ -50,6 +50,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'services',
+    'orders',
+    'carts',
+    'appointments',
+    'users',
 ]
 
 MIDDLEWARE = [
--- clean_services_platform/manage.py
+++ clean_services_platform/manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clean_services_platform.settings')
